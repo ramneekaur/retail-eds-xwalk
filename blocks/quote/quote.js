3b392f2d5a2c7c4f0af0320208e16ebe2c478665
Ramneek Kaur <ramneekk@adobe.com>
2025-01-24 09:54:02 +1100

    updated site path

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -2,6 +2,5 @@ export default function decorate(block) {
   const [quoteWrapper] = block.children;
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
-  blockquote.style.backgroundColor = quoteWrapper.
   quoteWrapper.replaceChildren(blockquote);
   }
